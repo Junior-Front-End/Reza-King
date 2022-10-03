ef3585808c87124b44ba4db8818efea5113e8412
Shervin <khanim97@gmail.com>
2022-10-03 18:42:40 +0330

    c

--- example-1-total-challenge/README.md
+++ example-1-total-challenge/README.md
@@ -1,10 +1,19 @@
 # Total Challenge
 2022.10.03 | by khanim97@gmail.com
 
+## Links
 
-How to execute all functions in one function?
+* [watch video](https://archive.org/download/total-challenge/total-challenge.mp4)
+(if the link does not work try this link: [overview video](https://archive.org/details/total-challenge))
+
+* [github repository](https://github.com/Junior-Front-End/Reza-King)
+
+
+## How to execute all functions in one function?
 * Solution 1: Simple function! 
 * Solution 1-1: Combine All logs!
 * Solution 2: Create a class!  
+
+## Result
 
 ![](screenshot.png)
--- example-1-total-challenge/app/js/solution-1-1.js
+++ example-1-total-challenge/app/js/solution-1-1.js
@@ -11,50 +11,41 @@ function SolutionOneOne() {
     // Array 
     var array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
+    //
+    var primaryValue = {
+        total: 0,
+        odds: 0,
+        evens: 0
+    };
 
     // (challenge 1; Total of Array)
-    var total = array.reduce((cur, item) => {
-        cur += item;
-        return cur;
-    }, 0);
- 
+    var finalValues = array.reduce((cur, item) => {
 
-    // (challenge 2): odd
-    var odds = array.reduce((cur, item) => {
-        if (item % 2 == 1) cur += item;
-        return cur;
-    }, 0);
- 
+        //
+        cur["total"] += item;
 
-    // (challenge 3): even
-    var evens = array.reduce((cur, item) => {
-        if (item % 2 == 0) cur += item;
-        return cur;
-    }, 0);
- 
-    
-    // log all
-    console.log(`
-    ---------------------------------------
-                Solution 1-1
-    ---------------------------------------
-    (1) Sum of Array: ${total}
-    (2) Sum of Odds: ${odds}
-    (3) Sum of Evens: ${evens}
-    (4) Sum - odds: ${total - odds}
-    (5) Sum + evens: ${total + evens}
-    ---------------------------------------
-    `);
+        //
+        if (item % 2) 
+        cur["evens"] += item;
+        else 
+        cur["odds"] += item;
 
-    
+        //
+        return cur;
+
+    }, primaryValue);
+  
+ 
+    //
     var result = {
-        total: total,
-        odds: odds,
-        evens: evens,
-        SumMinusOdds: total - odds,
-        Sum_plus_evens: total + evens
+        total: finalValues["total"],
+        odds: finalValues["odds"],
+        evens: finalValues["evens"],
+        SumMinusOdds: finalValues["total"] - finalValues["odds"],
+        Sum_plus_evens: finalValues["total"] + finalValues["evens"]
     }
 
+    //
     console.log(result);
 
 }
